ed876611aa141e13aff7eef45ff96d4bc2a28d8c
nicanico <nicsousan@gmail.com>
2023-09-11 00:23:16 -0300

    11/09 - validações de campos númericos

--- front-web-kalos-academia/src/pages/ForgotPass/ForgotPass.jsx
+++ front-web-kalos-academia/src/pages/ForgotPass/ForgotPass.jsx
@@ -10,11 +10,20 @@ export const ForgotPass = () => {
   const [tokenThree, setTokenThree] = useState(0)
   const [tokenFour, setTokenFour] = useState(0)
   const [tokenFive, setTokenFive] = useState(0)
+  const [disabled, setDisabled] = useState('disabled')
   console.log(tokenOne, tokenTwo, tokenThree, tokenFour, tokenFive)
 
-  const validateToken = () => {
+  
 
+  const validateToken = () => {
+    if(tokenOne == '' || tokenTwo == '' || tokenThree == '' || tokenFour == '' || tokenFive == ''){
+      console.log('erro')
+    } else {
+      console.log('ok')
+    }
   }
+
+  validateToken()
 
   const userEmail = localStorage.getItem('userEmail')
 
@@ -25,21 +34,21 @@ export const ForgotPass = () => {
         <p>Por favor verifique o código de 5 dígitos que foi enviado para o e-mail <span className='userEmail'>{userEmail}</span> para efetuar a troca da senha.</p>
       </div>
       <div className='forgot_passowrd_token_fiels'>
-        <InputNumber size="large" min={1} max={9} defaultValue={''} value={tokenOne} onChange={e => setTokenOne(e)} />
-        <InputNumber size="large" min={1} max={9} defaultValue={''} value={tokenTwo} onChange={e => setTokenTwo(e)} />
-        <InputNumber size="large" min={1} max={9} defaultValue={''} value={tokenThree} onChange={e => setTokenThree(e)} />
-        <InputNumber size="large" min={1} max={9} defaultValue={''} value={tokenFour} onChange={e => setTokenFour(e)} />
-        <InputNumber size="large" min={1} max={9} defaultValue={''} value={tokenFive} onChange={e => setTokenFive(e)} />
+        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenOne} onChange={e => setTokenOne(e)} />
+        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenTwo} onChange={e => setTokenTwo(e)} />
+        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenThree} onChange={e => setTokenThree(e)} />
+        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenFour} onChange={e => setTokenFour(e)} />
+        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenFive} onChange={e => setTokenFive(e)} />
       </div>
       <div className='forgot_passowrd_new_fiels'>
         <div className='fiels'>
           <div className="new_passowrd">
             <p className='textNameForInput'>Nova Senha</p>
-            <Input size="large" disabled />
+            <Input size="large" disabled={disabled} />
           </div>
           <div className="repeat_passowrd">
             <p className='textNameForInput'>Repetir Senha</p>
-            <Input size="large" disabled />
+            <Input size="large" disabled={disabled} />
           </div>
         </div>
         < ButtonPrimary size={'large'} nameButton='Trocar' />
