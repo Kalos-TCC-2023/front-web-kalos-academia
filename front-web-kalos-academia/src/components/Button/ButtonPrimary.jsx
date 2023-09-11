12e9310ec31b74ab778e620db732c78e13df070a
NICOLE SOUZA SANTOS <22282218@dsn127m1241959.cfp127aluno.local>
2023-09-11 16:53:06 -0300

    11/09 - validaçao de tela de recuperaçao de senha completa

--- front-web-kalos-academia/src/components/Button/ButtonPrimary.jsx
+++ front-web-kalos-academia/src/components/Button/ButtonPrimary.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button } from 'antd';
 
-export const ButtonPrimary = ({nameButton, size, onClickFuction }) => {
+export const ButtonPrimary = ({nameButton, size, onClickFuction, disabled, shape }) => {
   return (
     <div>
-        <Button type="primary" size={size} block onClick={onClickFuction}>{nameButton}</Button>
+        <Button type="primary" shape={shape} disabled={disabled} size={size} block onClick={onClickFuction}>{nameButton}</Button>
     </div>
   )
 }
--- front-web-kalos-academia/src/pages/ForgotPass/ForgotPass.jsx
+++ front-web-kalos-academia/src/pages/ForgotPass/ForgotPass.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { InputNumber, Input } from 'antd';
 import { ButtonPrimary } from '../../components/Button/ButtonPrimary';
 import './ForgotPass.css'
@@ -10,20 +10,36 @@ export const ForgotPass = () => {
   const [tokenThree, setTokenThree] = useState(0)
   const [tokenFour, setTokenFour] = useState(0)
   const [tokenFive, setTokenFive] = useState(0)
+  const [tokenCode, setToken] = useState('')
   const [disabled, setDisabled] = useState('disabled')
-  console.log(tokenOne, tokenTwo, tokenThree, tokenFour, tokenFive)
 
-  
+  const refVerification = useRef()
+  const refFielsNewPassoword = useRef()
 
   const validateToken = () => {
-    if(tokenOne == '' || tokenTwo == '' || tokenThree == '' || tokenFour == '' || tokenFive == ''){
+    console.log(tokenCode)
+
+    if (tokenOne == null || tokenTwo == null || tokenThree == null || tokenFour == null || tokenFive == null) {
       console.log('erro')
+      refVerification.current.style.display = 'flex'
+
+    } else if (tokenOne !== 0 && tokenTwo !== 0 && tokenThree !== 0 && tokenFour !== 0 && tokenFive !== 0) {
+
+      refVerification.current.style.display = 'none'
+
+      const token = [tokenOne.toString(), tokenTwo.toString(), tokenThree.toString(), tokenFour.toString(), tokenFive.toString()]
+
+      const fullToken = token.reduce((letter, token) => letter + token)
+
+      setToken(fullToken)
+
+      refFielsNewPassoword.current.style.display = 'flex'
+
     } else {
-      console.log('ok')
+      refVerification.current.style.display = 'flex'
     }
   }
 
-  validateToken()
 
   const userEmail = localStorage.getItem('userEmail')
 
@@ -33,26 +49,37 @@ export const ForgotPass = () => {
         <h1>Insira o código de verificação</h1>
         <p>Por favor verifique o código de 5 dígitos que foi enviado para o e-mail <span className='userEmail'>{userEmail}</span> para efetuar a troca da senha.</p>
       </div>
-      <div className='forgot_passowrd_token_fiels'>
-        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenOne} onChange={e => setTokenOne(e)} />
-        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenTwo} onChange={e => setTokenTwo(e)} />
-        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenThree} onChange={e => setTokenThree(e)} />
-        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenFour} onChange={e => setTokenFour(e)} />
-        <InputNumber type='number' size="large" min={0} max={9} maxLength={1} defaultValue={''} value={tokenFive} onChange={e => setTokenFive(e)} />
-      </div>
-      <div className='forgot_passowrd_new_fiels'>
-        <div className='fiels'>
-          <div className="new_passowrd">
-            <p className='textNameForInput'>Nova Senha</p>
-            <Input size="large" disabled={disabled} />
-          </div>
-          <div className="repeat_passowrd">
-            <p className='textNameForInput'>Repetir Senha</p>
-            <Input size="large" disabled={disabled} />
-          </div>
+      <div className="fiels_verification">
+        <div className='forgot_passowrd_token_fiels'>
+          <InputNumber type='number' size="large" min={1} max={9} maxLength={1} defaultValue={''} value={tokenOne} onChange={e => setTokenOne(e)} />
+          <InputNumber type='number' size="large" min={1} max={9} maxLength={1} defaultValue={''} value={tokenTwo} onChange={e => setTokenTwo(e)} />
+          <InputNumber type='number' size="large" min={1} max={9} maxLength={1} defaultValue={''} value={tokenThree} onChange={e => setTokenThree(e)} />
+          <InputNumber type='number' size="large" min={1} max={9} maxLength={1} defaultValue={''} value={tokenFour} onChange={e => setTokenFour(e)} />
+          <InputNumber type='number' size="large" min={1} max={9} maxLength={1} defaultValue={''} value={tokenFive} onChange={e => setTokenFive(e)} />
         </div>
-        < ButtonPrimary size={'large'} nameButton='Trocar' />
+        <div ref={refVerification} className="verification">
+          Verifique se todos os campos foram preenchidos corretamente
+        </div>
+        <ButtonPrimary shape='round' size={'large'} nameButton='Validar' onClickFuction={validateToken} />
+
       </div>
+
+      <div ref={refFielsNewPassoword} className="visible_fiels">
+        <div  className='forgot_passowrd_new_fiels'>
+          <div className='fiels'>
+            <div className="new_passowrd">
+              <p className='textNameForInput'>Nova Senha</p>
+              <Input size="large" />
+            </div>
+            <div className="repeat_passowrd">
+              <p className='textNameForInput'>Repetir Senha</p>
+              <Input size="large" />
+            </div>
+          </div>
+          < ButtonPrimary size={'large'} nameButton='Trocar' />
+        </div>
+      </div>
+
     </div>
   )
 }
