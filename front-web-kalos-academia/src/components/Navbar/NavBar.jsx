cfc411852eb87f8443adf89ca5d3e660eb9df735
nicanico <nicsousan@gmail.com>
2023-09-25 20:25:39 -0300

    25/09 - configurando tooltips para menu

--- front-web-kalos-academia/src/components/Navbar/NavBar.jsx
+++ front-web-kalos-academia/src/components/Navbar/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Tooltip } from 'antd'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckSquare, faHome, faUser, faUsers, faBoxOpen, faDumbbell, faImages, faComments } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faUsers, faBoxOpen, faDumbbell, faImages, faComments } from '@fortawesome/free-solid-svg-icons'
 
 import './NavBar.css'
 import logo from './img/logo.png'
@@ -14,39 +15,53 @@ export const NavBar = () => {
                 <div className="nav_bar_links">
                     <FontAwesomeIcon icon="check-square" />
                     <Link to='/menu/home'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon className='homeIcon' icon={faHome} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Home">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon className='homeIcon' icon={faHome} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/perfil'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faUser} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Perfil">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faUser} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/alunos'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faUsers} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Alunos da academia">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faUsers} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/produtos'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faBoxOpen} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Produtos">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faBoxOpen} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/treinos'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faDumbbell} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Treinos da academia">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faDumbbell} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/posts'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faImages} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Postagem da academia">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faImages} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                     <Link to='/menu/chat'>
-                        <div className="icon_font_awesome">
-                            <FontAwesomeIcon icon={faComments} size='lg' style={{ color: '#33ffa7' }} />
-                        </div>
+                        <Tooltip placement="right" title="Chat da academia">
+                            <div className="icon_font_awesome">
+                                <FontAwesomeIcon icon={faComments} size='lg' style={{ color: '#33ffa7' }} />
+                            </div>
+                        </Tooltip>
                     </Link>
                 </div>
                 <p>Sair</p>
