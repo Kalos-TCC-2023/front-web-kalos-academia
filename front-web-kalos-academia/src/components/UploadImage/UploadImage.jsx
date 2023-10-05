0d6dd1bfd847db3a8f580929eaa1bf27678f6e04
nicanico <nicsousan@gmail.com>
2023-10-04 23:21:49 -0300

    04/10 - upload de imagem completo

--- front-web-kalos-academia/src/components/UploadImage/UploadImage.jsx
+++ front-web-kalos-academia/src/components/UploadImage/UploadImage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { PlusOutlined } from '@ant-design/icons';
-import { Modal, Upload, Button } from 'antd';
+import { Modal, Upload, Button, Input } from 'antd';
 import './UploadImage.css'
 import { storage } from '../../adapters/firebase';
 import { uploadBytesResumable, getDownloadURL, ref } from 'firebase/storage';
@@ -10,61 +10,33 @@ export const UploadImage = ({ fileList, setFileList, imageDb, setImageDb, update
 
     const [imgUrl, setImgUrl] = useState('')
 
-    const getBase64 = (file) =>
-        new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            console.log('header', reader)
-            reader.onerror = (error) => reject(error);
-        })
+    const handleChange = (event) => {
+        event.preventDefault()
 
-    const [previewOpen, setPreviewOpen] = useState(false);
-    const [previewImage, setPreviewImage] = useState('');
-    const [previewTitle, setPreviewTitle] = useState('');
+        const file = event.target[0]?.files[0]
+        console.log(file)
+        if (!file) return console.log('error')
 
+        const storageRef = ref(storage, `images/${file.name}`)
+        const uploadTask = uploadBytesResumable(storageRef, file)
 
-    const handleCancel = () => setPreviewOpen(false)
-    const handlePreview = async (file) => {
+        uploadTask.on(
+            "state_changed",
+            snapshot => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                console.log(progress)
+            },
+            error => {
+                console.log(error)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then(url => {
+                    setImgUrl(url)
+                    setImageDb(url)
+                })
+            }
+        )
 
-        if (!file.url && !file.preview) {
-            console.log(file)
-            file.preview = await getBase64(file.originFileObj);
-        }
-        setPreviewImage(file.url || file.preview);
-        setPreviewOpen(true);
-        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
-    }
-
-    const handleChange = ({ fileList }) => {
-        setFileList(fileList)
-        if (fileList[0] === undefined) {
-            return
-        } else {
-
-            if (!fileList) return
-            fileList[0].status = 'done'
-            const file = fileList[0]
-            const storageRef = ref(storage, `images/${file.name}`)
-            const uploadTask = uploadBytesResumable(storageRef, file)
-
-            uploadTask.on(
-                "state_changed",
-                snapshot => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                    console.log(progress)
-                },
-                error => {
-                    console.log(error)
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then(url => {
-                        setImgUrl(url)
-                        setImageDb(url)
-                    })
-                }
-            )
-        }
     }
 
     console.log(imgUrl)
@@ -86,7 +58,11 @@ export const UploadImage = ({ fileList, setFileList, imageDb, setImageDb, update
     return (
 
         <div className='container_icon'>
-            <div className="upload_icon">
+
+
+
+
+            {/* <div className="upload_icon">
                 <Upload style={'width: 100%; height: 100%;'}
                     listType="picture-circle"
                     fileList={fileList}
@@ -106,9 +82,14 @@ export const UploadImage = ({ fileList, setFileList, imageDb, setImageDb, update
                         src={imageDb}
                     />
                 </Modal>
-            </div>
-
-            {/* <Button onClick={handleChange} type="primary" shape="round" size='small'></Button> */}
+            </div> */}
+            {imageDb && <img src={imageDb} height={100} className='img_upload' />}
+            <form onSubmit={handleChange}>
+                <input type='file' />
+                <button type='submit'>Enviar</button>
+            </form>
+            <br />
+            
 
         </div>
     )
