2d96a8b46204e07b966e9bdd52b43dfbc7a3f7b9
nicanico <nicsousan@gmail.com>
2023-11-13 21:14:26 -0300

    13/11 - nova pagina de editar aluno da academia e ajustes de rotas

--- front-web-kalos-academia/src/components/UploadImgGym/UploadImgGym.jsx
+++ front-web-kalos-academia/src/components/UploadImgGym/UploadImgGym.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-export const UploadImgGym = () => {
+export const UploadImgGym = ({ setImg }) => {
+
+  const [selectFileName, setSelectFileName] = useState('')
+
+  const handleFileChange = (e) => {
+    const selectFileName = e.target.files[0];
+
+    if (selectFileName) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const fileDataURL = event.target.result;
+        setImg(fileDataURL)
+      };
+
+      reader.readAsDataURL(selectFileName);
+    }
+  }
+
+  const fileInput = useRef()
+
+
   return (
-    <div>UploadImgGym</div>
+    <div className="container-change-file">
+    <div className="change-file" onClick={() => fileInput.click()}>
+      <p>Escolher arquivos</p>
+    </div>
+    <div className="changed-file">
+      <p>{selectFileName ? selectFileName.name : 'Nenhum arquivo escolhido'}</p>
+    </div>
+    <input
+      type="file"
+      style={{ display: 'none' }}
+      ref={fileInput}
+      onChange={handleFileChange}
+    />
+  </div>
   )
 }
